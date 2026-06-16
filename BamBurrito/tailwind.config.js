b7bac6bf34fd45e8f2e8f7f6ad9911d15c0eaa1a
16 Anton Nordin <nordinen93@hotmail.com>
2026-06-16 19:41:10 +0200

    changed navbar font to treuno

--- BamBurrito/tailwind.config.js
+++ BamBurrito/tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         extend: {
             colors: {
                 bamorange: '#e64e1c',
+            },
+            fontFamily: {
+                bangers: ['Bangers', 'system-ui'],
+                trueno: ['Trueno', 'orig_trueno_round_bold', 'system-ui'],
+                anton: ['Anton', 'system-ui'],
             }
         },
     },
