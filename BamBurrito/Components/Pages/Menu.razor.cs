8d0d2e910d13fc0db622e89ba6aedfe34a2e5abb
16 Anton Nordin <nordinen93@hotmail.com>
2026-06-17 15:59:50 +0200

    fixed some bugs with duplicate files. concept of menupage done

---
+++ BamBurrito/Components/Pages/Menu.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using BamBurrito.Core.Entities;
+
+namespace BamBurrito.Components.Pages
+{
+    public partial class Menu
+    {
+        protected List<MenuItem> AllMenuItems { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            AllMenuItems = new List<MenuItem>
+            {
+                new MenuItem { Id = 1, Name = "Asado", Price = 139, Category = "Burritos", Description = "Flankstek, majs, ost, avocado, paprika, chimichurri och couscous.", IsAvailable = true },
+                new MenuItem { Id = 2, Name = "Pollito", Price = 129, Category = "Burritos", Description = "Kyckling, majs, ost, avocado, pebre, ris och paprika.", IsAvailable = true },
+                new MenuItem { Id = 3, Name = "Frijoles", Price = 115, Category = "Burritos", Description = "Svarta bönor, majs, ost, avocado, pebre, ris och paprika.", IsAvailable = true },
+
+                new MenuItem { Id = 4, Name = "Klassisk Läsk / Loka", Price = 25, Category = "Dryck", Description = "Coca-Cola, Sprite, Fanta, Trocadero eller Loka.", IsAvailable = true },
+                new MenuItem { Id = 5, Name = "Jarritos Premium", Price = 29, Category = "Dryck", Description = "Mexikansk kultläsk – Välj mellan Mango, Jordgubb eller Ananas.", IsAvailable = true }
+            };
+        }
+    }
+}
