9a955d9a6bf5bf00ba6501001b01c690779f412f
16 Anton Nordin <nordinen93@hotmail.com>
2026-06-17 14:49:34 +0200

    separated html and c# code for better separations of concern

---
+++ BamBurrito/Components/Pages/NavMenu.razor.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.JSInterop;
+
+namespace BamBurrito.Components.Layout
+{
+    public partial class NavMenu : IDisposable
+    {
+        // Vi injicerar tjänsterna här på ett snyggt, rent C#-vis istället för @inject
+        [Inject] public NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] public IJSRuntime JSRuntime { get; set; } = null!;
+
+        private string? currentUrl;
+        private bool collapseNavMenu = true;
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+
+        private void CloseNavMenu()
+        {
+            collapseNavMenu = true;
+        }
+
+        protected override void OnInitialized()
+        {
+            currentUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
+            NavigationManager.LocationChanged += OnLocationChanged;
+        }
+
+        private void OnLocationChanged(object? sender, LocationChangedEventArgs e)
+        {
+            currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
+            collapseNavMenu = true;
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
+    }
+}
