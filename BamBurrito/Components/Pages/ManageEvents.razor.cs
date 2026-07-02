e35ed43d2d59cd2e11c1902e398b956e8cd620ae
16 Anton Nordin <nordinen93@hotmail.com>
2026-07-02 19:41:15 +0200

    Add event image upload and calendar image display
    Added support for uploading and previewing event background images in ManageEvents.razor. Updated event submission to save images to /images/events and store their paths. Enhanced Places.razor calendar to show event days with background images. Improved UI elements and extended Tailwind CSS utilities for new styles. Added a sample event image to the repository.

--- BamBurrito/Components/Pages/ManageEvents.razor.cs
+++ BamBurrito/Components/Pages/ManageEvents.razor.cs
@@ -4,6 +4,9 @@ using BamBurrito.Core.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace BamBurrito.Components.Pages;
 
@@ -13,10 +16,12 @@ public partial class ManageEvents
     [Inject] protected NavigationManager Navigation { get; set; } = default!;
     [Inject] protected IConfiguration Configuration { get; set; } = default!;
     [Inject] protected IJSRuntime JS { get; set; } = default!;
+    [Inject] protected IWebHostEnvironment Env { get; set; } = default!;
 
     protected LocationEvent? newEvent { get; set; }
     protected List<LocationEvent> events = new();
     protected bool isAuthorized = false;
+    private IBrowserFile? selectedFile;
 
     private DateTime GetCleanNow()
     {
@@ -51,10 +56,29 @@ public partial class ManageEvents
         }
     }
 
+    protected void HandleFileSelected(InputFileChangeEventArgs e)
+    {
+        selectedFile = e.File;
+    }
+
     protected async Task HandleSubmit()
     {
         if (newEvent != null)
         {
+            if (selectedFile != null)
+            {
+                var folderPath = Path.Combine(Env.WebRootPath, "images", "events");
+                Directory.CreateDirectory(folderPath); 
+
+                var fileName = $"{Guid.NewGuid()}_{selectedFile.Name}";
+                var filePath = Path.Combine(folderPath, fileName);
+
+                await using var stream = new FileStream(filePath, FileMode.Create);
+                await selectedFile.OpenReadStream(maxAllowedSize: 10485760).CopyToAsync(stream); 
+
+                newEvent.ImagePath = $"/images/events/{fileName}";
+            }
+
             newEvent.StartTime = new DateTime(newEvent.StartTime.Year, newEvent.StartTime.Month, newEvent.StartTime.Day, newEvent.StartTime.Hour, newEvent.StartTime.Minute, 0);
             newEvent.EndTime = new DateTime(newEvent.EndTime.Year, newEvent.EndTime.Month, newEvent.EndTime.Day, newEvent.EndTime.Hour, newEvent.EndTime.Minute, 0);
 
@@ -68,6 +92,7 @@ public partial class ManageEvents
 
         var cleanNow = GetCleanNow();
         newEvent = new LocationEvent { StartTime = cleanNow, EndTime = cleanNow.AddHours(4) };
+        selectedFile = null;
         StateHasChanged();
     }
 
